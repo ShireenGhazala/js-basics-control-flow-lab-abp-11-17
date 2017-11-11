2192d27a98029c8e11a4116a6b5beac127a0e4be
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-11 18:12:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function scuberGreetingForFeet(feet) {
   If (feet===400 || feet<400){
     return "This one is on me!"
     }
- elseif(feet>2000 && feet<2500){
+ else if(feet>2000 && feet<2500){
     return "I will gladly take your thirty bucks."
  }
- elseif(feet>2500){
+ else if(feet>2500){
    return "No can do."
  }
 }
